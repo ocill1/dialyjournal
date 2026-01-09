5aa810fd0863f48829595fd0a9e11441f7d2fa68
ocill1 <ocilnaif503@gmail.com>
2026-01-10 02:26:38 +0700

    ubah judul from login

--- login.php
+++ login.php
@@ -82,7 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="card-body">
           <div class="text-center mb-3">
             <i class="bi bi-person-circle h1 display-4"></i>
-            <p>My Daily Journal</p>
+            <p>Welcomce to My Dialy Journal</p>
             <hr />
           </div>
           <form action="" method="post">
